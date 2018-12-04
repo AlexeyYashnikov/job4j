0cfd1d2c667b058de6b730eeafbc8775804f9776
AlexeyYashnikov <AYashnikov@yandex.ru>
2018-12-04 21:00:02 +0300

    1.3. JavaDoc. Code convention[#88826]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,21 @@
 package ru.job4j.calculate;
 
-public class Calculate {
-
+/**
+ * Class Демо класс Hello World!
+ * @author Alex Yashnikov
+ * @since 2018-12-04
+ * @version 0,1
+*/
+public class Calculate 
+/**
+ * Main
+ * @param args - аргументы
+*/
     public static void main(String[] args) {
+/**
+ * Method println
+ * @return string Hello World!
+*/
     System.out.println("Hello World!");
     }
 }
