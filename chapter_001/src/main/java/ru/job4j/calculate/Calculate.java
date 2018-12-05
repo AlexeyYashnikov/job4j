5ea64dde7c4bf05d9677c8f13bbed60cbc957046
AlexeyYashnikov <AYashnikov@yandex.ru>
2018-12-05 22:08:33 +0300

    1.4. JUnit[#88827]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,22 +1,28 @@
 package ru.job4j.calculate;
 
 /**
- * Class Демо класс Hello World!
+ * Class Demo class Hello World!
  * @author Alex Yashnikov
  * @since 2018-12-04
- * @version 0,1
+ * @version 0.1
 */
 public class Calculate {
 /**
  * Main
- * @param args - аргументы
+ * @param args - arguments
 */
     public static void main(String[] args) {
 		
-/**
- * Method println
- * @return string Hello World!
-*/
-    System.out.println("Hello World!");
-    }
+		System.out.println("Hello World!");
+	
+		/**
+		* Method echo.
+		* @param name Alex Yashnikov.
+		* @return Echo plus Alex Yashnikov.
+		*/
+		public String echo(String name) {
+			return "Echo, echo, echo : " + name;
+		}
+	
+	}
 }
