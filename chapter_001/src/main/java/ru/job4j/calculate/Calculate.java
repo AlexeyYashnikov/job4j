aec71b23eb34da1b6d3a6d50ae8f44a7ab7010b0
AlexeyYashnikov <AYashnikov@yandex.ru>
2018-12-08 16:55:29 +0300

    2.0. Видео[#88787] - test Git from IDEA - 2

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,7 +8,7 @@ package ru.job4j.calculate;
 public class Calculate {
 /**
  * Main method.
- * @param args - arguments
+ * @param args - arguments.
 */
     public static void main(String[] args) {
 		System.out.println("Hello World!");
